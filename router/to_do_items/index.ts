fb2422aa51f95f9fac71d48a7cb0a4fe71311da4
harryuan65 <harryuan.65@gmail.com>
2023-01-29 10:05:19 +0800

    feat: delete

--- router/to_do_items/index.ts
+++ router/to_do_items/index.ts
@@ -29,8 +29,16 @@ router.patch("/:id", async (req: Request<IToDoItemData>, res: Response ) => {
   }
 })
 
-router.delete("/:id", (req: Request<IToDoItemData>, res: Response ) => {
-  res.send(`delete to do items${req.params.id}`)
+router.delete("/:id", async (req: Request<IToDoItemData>, res: Response ) => {
+  const id = req.params.id;
+  const item = await Manager.findOne(ToDoItem, {where: {id}});
+
+  if(item) {
+    await Manager.delete(ToDoItem, item);
+    res.send("OK");
+  } else {
+    res.status(404).send(`id: ${id} Not found!`)
+  }
 })
 
 export default router;
