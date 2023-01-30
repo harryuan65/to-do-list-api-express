123d31bed8cfd0ccddc356a4979eb6a4612ace28
harryuan65 <harryuan.65@gmail.com>
2023-01-30 22:49:19 +0800

    feat: add logger

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from "express";
-import {Database} from "./models/database";
-import router from "./router";
-import bodyParser from "body-parser";
+import express, { Express, Request, Response } from 'express';
+import { Database } from './models/database';
+import router from './router';
+import bodyParser from 'body-parser';
 
-if (process.env.ENV !== "production") {
-  const dotenv = require("dotenv");
+if (process.env.ENV !== 'production') {
+  const dotenv = require('dotenv');
   dotenv.config();
 }
 
@@ -12,17 +12,22 @@ const app: Express = express();
 
 const PORT = process.env.PORT || 3003;
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
 
+app.use((req: Request, res: Response, next) => {
+  console.log(`[${res.statusCode}] ${req.method.toUpperCase()} ${req.path}`);
+  next();
+});
+
 app.use(router);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Healthy")
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('Healthy');
+});
 
 app.listen(PORT, () => {
-  console.log(`[express] 🚀 Listening on ${PORT}`)
-  console.log(`[database] 🚀 Setting up database ${Database.driver.database}`)
-})
+  console.log(`[express] 🚀 Listening on ${PORT}`);
+  console.log(`[database] 🚀 Setting up database ${Database.driver.database}`);
+});
